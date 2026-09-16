a31691551c62a5138e5d2126c96a4bdb9c88a1b2
dogadin.anton2 <dogadin.anton2@rwb.ru>
2026-09-16 11:32:31 +0300

    add copy plugin

--- library/webpack.config.js
+++ library/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const COMMON_SASS_PATH = path.resolve(__dirname, './src/common/sass')
 
@@ -33,7 +34,15 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css'
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/common/img/jpg'),
+          to: path.resolve(__dirname, 'dist/assets')
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
